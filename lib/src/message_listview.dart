002b905225caf94725f3793f137421031a4dbe9a
Sijan Gurung <gurung.sijan@gmail.com>
2020-06-05 21:25:23 +0200

    the list is always reverse, so that the scroll is always to the bottom !

--- lib/src/message_listview.dart
+++ lib/src/message_listview.dart
@@ -136,7 +136,7 @@ class _MessageListViewState extends State<MessageListView> {
                 ListView.builder(
                   controller: widget.scrollController,
                   shrinkWrap: true,
-                  reverse: widget.inverted,
+                  reverse: true,
                   itemCount: widget.messages.length,
                   itemBuilder: (context, i) {
                     bool showAvatar = shouldShowAvatar(i);
